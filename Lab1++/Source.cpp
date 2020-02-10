3c9f4dba6aeb9ae537219e228e5ebd17d216d562
Hard <Khmelinin2001@gmail.com>
2020-02-10 22:11:33 +0200

    1

--- Lab1++/Source.cpp
+++ Lab1++/Source.cpp
@@ -1,1 +1,15 @@
 #include <iostream>
+
+using namespace std;
+
+int main() 
+{
+
+
+	int x = 148;
+	/*cin >> x;*/
+	for (i = sizeof(x) * 8 - 1; i >= 0; --i)
+	{
+		cout << (int)((x >> i) & 1);
+	}
+}
