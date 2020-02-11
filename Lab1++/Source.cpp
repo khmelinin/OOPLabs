38863cfba571a9b040fed428e4bdacc9d8b18832
khmelinin <khmelinin2001@gmail.com>
2020-02-11 22:14:50 +0200

    finally 2

--- Lab1++/Source.cpp
+++ Lab1++/Source.cpp
@@ -7,6 +7,7 @@ void PlusOne(int& a)
 {
     for (int i = 1; ((a ^= i) & i) == 0; i <<= 1);
 }
+
 int main()
 {
     int a = -43;
--- Lab1Sharp/Program.cs
+++ Lab1Sharp/Program.cs
@@ -4,13 +4,14 @@ namespace Lab1Sharp
 {
     class Program
     {
-        static void PlusOne(ref int a, ref int b)
+        static void PlusOne(ref int a)
         {
+            for (int i = 1; ((a ^= i) & i) == 0; i <<= 1) ;
         }
         static void Main(string[] args)
         {
-            int a = 43, b = 1;
-            PlusOne(ref a, ref b);
+            int a = -43;
+            PlusOne(ref a);
            
         }
     }
