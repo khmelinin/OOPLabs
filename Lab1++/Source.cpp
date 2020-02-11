d9e18c50ff160a0591cbcf661833c1c59d128a4d
khmelinin <khmelinin2001@gmail.com>
2020-02-11 22:41:53 +0200

    1

--- Lab1++/Source.cpp
+++ Lab1++/Source.cpp
@@ -8,10 +8,17 @@ void PlusOne(int& a)
     for (int i = 1; ((a ^= i) & i) == 0; i <<= 1);
 }
 
+bool Camparing(int a, int b)
+{
+    return a & b;
+}
+
 int main()
 {
     int a = -43;
+    int b = 75;
     PlusOne(a);
-    cout << a << endl;
 
+    cout << a << endl;
+    cout << "Camparing = " << Camparing(a, b) << endl;
 }
