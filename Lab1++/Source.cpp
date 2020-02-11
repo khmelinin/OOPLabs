206e44693c35d6906552bdd1d48999a5dbbb17cc
khmelinin <khmelinin2001@gmail.com>
2020-02-11 22:08:09 +0200

    finally

--- Lab1++/Source.cpp
+++ Lab1++/Source.cpp
@@ -3,35 +3,14 @@
 
 using namespace std;
 
-void PlusOne(int &a, int &b)
+void PlusOne(int& a)
 {
-	 a=a ^ b;
+    for (int i = 1; ((a ^= i) & i) == 0; i <<= 1);
 }
-
 int main()
 {
-	//int x = 1;
-	///*cin >> x;*/
-	//if (x < 0)
-	//{
-	//	x = x * (-1);
-	//	cout << "new x = " << x << endl;
-	//	cout << "1";
-	//	for (int i = sizeof(x) * 8 - 1; i >= 0; --i)
-	//	{
-	//		if (i != 31)
-	//			cout << (int)((x >> i) & 1);
-	//	}
-	//}
-	//if(x>=0)
-	//{
-	//	for (int i = sizeof(x) * 8 - 1; i >= 0; --i)
-	//	{
-	//		cout << (int)((x >> i) & 1);
-	//	}
-	//}
-	
-	int a = -43, b=1;
-	PlusOne(a, b);
+    int a = -43;
+    PlusOne(a);
+    cout << a << endl;
 
 }
--- Lab1Sharp/Program.cs
+++ Lab1Sharp/Program.cs
@@ -6,11 +6,10 @@ namespace Lab1Sharp
     {
         static void PlusOne(ref int a, ref int b)
         {
-            a=a^b;
         }
         static void Main(string[] args)
         {
-            int a = -43, b = 1;
+            int a = 43, b = 1;
             PlusOne(ref a, ref b);
            
         }
