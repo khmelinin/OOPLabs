b15af6ac01614199072ce91594fef25327178e2e
khmelinin <khmelinin2001@gmail.com>
2020-02-13 12:12:04 +0200

    1

--- Lab1++/Source.cpp
+++ Lab1++/Source.cpp
@@ -8,7 +8,7 @@ bool Comparing(int a, int b);
 
 int main()
 {
-    int a = -43, aa = 75, aaa=63;
+    int a = -43, aa = 75, aaa = 63;
     int b = 48,  bb = 69, bbb = 24;
     int c = 48,  cc = 78, ccc = 25;
 
@@ -27,22 +27,31 @@ int main()
 
 void PlusOne(int& a)
 {
+    int i = 1;
     for (int i = 1; ((a ^= i) & i) == 0; i <<= 1);
+    int ok;
+    ok = a;
+  /*  while ((a ^= i) & i == 0)
+    {
+        i <<= 1;
+    }*/
 }
 
 bool Comparing(int a, int b)
 {
     int aa, bb, ii;
-    for (int i = 0; i < sizeof(a)*8; i++)
+    for (int i = sizeof(a)*8; i > 0; i--)
     {
-        ii = 1 << i;
+        ii = 1 >> i;
         aa = a & ii;
         bb = b & ii;
         if(aa^bb)
         {
+            bool ok = 0;
             return 0;
         }
     }
+    bool ok = 1;
     return 1;
     /*return (a ^ b) == 0;*/
 }
