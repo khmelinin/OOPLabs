05a1bd6805dc08a312db0a2b43b41c7640277f9c
khmelinin <khmelinin2001@gmail.com>
2020-02-24 22:10:06 +0200

    cant learn c#

---
+++ Lab2SharpLib/Str.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab2SharpLib
+{
+    public static class Str
+    {
+        private static int size = 0;
+        private static char[] str;
+        static Str()
+        {
+            size = 0;
+            str = new char[size];
+        }
+        Str(char[]s)
+        {
+            size = 
+            str = new char[size];
+            for (int i = 0; i < size; i++)
+            {
+                str[i] = s[i];
+            }
+        }
+    }
+}
