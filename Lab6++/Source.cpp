b5c93c33a486ae2567f706d71849e9b8834a0fd7
Hard <khmelinin2001@gmail.com>
2020-04-29 12:45:44 +0300

    Lab6

---
+++ Lab6++/Source.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include "math.h"
+
+using namespace std;
+
+class Expression
+{
+    double a;
+    double b;
+    double c;
+public:
+    Expression(int aa, int bb, int cc)
+    {
+        a = aa;
+        b = bb;
+        c = cc;
+    }
+    double getA() { return a; }
+    double getB() { return b; }
+    double getC() { return c; }
+
+    void setA(int x) { a=x; }
+    void setB(int x) { b=x; }
+    void setC(int x) { c=x; }
+
+    double Result()
+    {
+        double res = 0;
+
+        res = Dividing((8 * Log10(b - 1) - c), (a * 2 + Dividing(b, c)));
+
+        return res;
+    }
+
+    double Log10(double a)
+    {
+        double res = 0;
+        try
+        {
+            if (a <= 0)
+                throw runtime_error("Log10 Error");
+            res = log10(a);
+        }
+        catch (runtime_error e)
+        {
+            cout << e.what() << endl;
+        }
+        return res;
+    }
+
+    double Dividing(double a, double b)
+    {
+        double res = 0;
+
+        try
+        {
+            if (b == 0)
+                throw runtime_error("dividing by zero");
+            res = a / b;
+        }
+        catch (runtime_error e)
+        {
+            cout << e.what() << endl;
+        }
+        return res;
+    }
+
+    double operator /(double b)
+    {
+        double res = 0;
+
+        try
+        {
+            if (b == 0)
+                throw runtime_error("dividing by zero (operator)");
+            res = a / b;
+        }
+        catch (runtime_error e)
+        {
+            cout << e.what() << endl;
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    Expression a[] = { Expression(1,1,1), Expression(1,2,0), Expression(1,1,0) };
+
+    cout << "a[0].Result() = " << a[0].Result() << endl;
+    cout << "a[1].Result() = " << a[1].Result() << endl;
+    cout << "a[2].Result() = " << a[2].Result() << endl;
+}
+
---
+++ Lab6Sharp/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Lab6Sharp
+{
+    
+    class Expression
+    {
+        double a;
+        double b;
+        double c;
+        public Expression(int aa,int bb,int cc)
+        {
+            a = aa;
+            b = bb;
+            c = cc;
+        }
+
+        public double A { get => a; set => a = value; }
+        public double B { get => b; set => b = value; }
+        public double C { get => c; set => c = value; }
+
+        public double Result()
+        {
+            double res = 0;
+
+            res = Dividing((8 * log10(b - 1) - c),(a * 2 + Dividing(b,c)));
+               
+            return res;
+        }
+        public double log10(double a)
+        {
+            double res = 0;
+            try
+            {
+                if (a <= 0)
+                throw new Exception("log10 Error");
+                res = Math.Log10(a);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return res;
+        }
+        private double Dividing(double a, double b)
+        {
+            double res = 0;
+
+            try
+            {
+                if (b == 0)
+                    throw new Exception("dividing by zero");
+                res = a / b;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return res;
+        }
+        public static double operator /(Expression a, double b)
+        {
+            double res = 0;
+            
+            try
+            {
+                if (b == 0)
+                throw new Exception("dividing by zero (operator)");
+                res = a.a / b;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return res;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Expression []a = { new Expression(1,1,1), new Expression(1,2,0),new Expression(1,1,0)};
+
+            Console.WriteLine("a[0].Result() = {0} \n",a[0].Result());
+            Console.WriteLine("a[1].Result() = {0} \n",a[1].Result());
+            Console.WriteLine("a[2].Result() = {0} \n",a[2].Result());
+        }
+    }
+}
